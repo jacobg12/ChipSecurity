96c516153accca59ee788eee8a3f39d00fb5c97c
jacobg12 <jacob.gaiski@comcast.net>
2023-07-01 23:18:06 -0400

    typo

--- Security/MainFrame.cs
+++ Security/MainFrame.cs
@@ -42,7 +42,7 @@ namespace ChipSecuritySystem.Security
                 InputChips();
                 PerformCheck();
             }
-            catch (ChipListNullOrEmpty chipListNullorEmpty) //Custom exception check to determine a determined error thrown
+            catch (ChipListNullOrEmpty chipListNullorEmpty) //Custom exception check to determine a pre-determined error thrown
             {
                 Console.WriteLine("No chips are present. Press any key to restart...");
                 Console.ReadKey();
